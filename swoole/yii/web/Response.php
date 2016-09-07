25981c18cda225a1877f97def88770d9b5a5dd77
lusc <lusc@jiedaibao.com>
2016-09-07 23:36:53 +0800

    fix bugs, Response::getCookies() return null

--- swoole/yii/web/Response.php
+++ swoole/yii/web/Response.php
@@ -13,6 +13,7 @@ use swoole\SwooleServer;
 use yii;
 use yii\base\InvalidConfigException;
 use yii\base\InvalidParamException;
+use yii\web\CookieCollection;
 use yii\web\HeaderCollection;
 use yii\web\ResponseFormatterInterface;
 
@@ -217,6 +218,34 @@ class Response extends yii\web\Response
             }
         }
         $this->sendCookies();
+    }
+
+    /**
+     * Returns the cookie collection.
+     * Through the returned cookie collection, you add or remove cookies as follows,
+     *
+     * ```php
+     * // add a cookie
+     * $response->cookies->add(new Cookie([
+     *     'name' => $name,
+     *     'value' => $value,
+     * ]);
+     *
+     * // remove a cookie
+     * $response->cookies->remove('name');
+     * // alternatively
+     * unset($response->cookies['name']);
+     * ```
+     *
+     * @return CookieCollection the cookie collection.
+     */
+
+    public function getCookies()
+    {
+        if ($this->_cookies === null) {
+            $this->_cookies = new CookieCollection;
+        }
+        return $this->_cookies;
     }
 
     /**
